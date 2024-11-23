bfe5e7a1c4d6fb872d40245d791d1feeace6f649
Sayid <sayid.salizhanov@mail.ru>
2024-11-23 15:40:35 +0300

    ajax

--- src/main/java/ru/itis/testwork1/servlet/WeatherServlet.java
+++ src/main/java/ru/itis/testwork1/servlet/WeatherServlet.java
@@ -16,17 +16,27 @@ public class WeatherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("weather.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String city = req.getParameter("town");
         String apiKey = "ec6ae61f58c44f36d3bd7d4f99c9993a";
 
-        String temperature = weatherService.getWeather((String) req.getSession().getAttribute("login"), city, apiKey);
-
-        req.setAttribute("temperature", temperature);
-        req.getRequestDispatcher("weather.jsp").forward(req, resp);
+        if (city == null || city.isEmpty()) {
+            req.getRequestDispatcher("weather.jsp").forward(req, resp);
+        }
+        else {
+            String temperature = weatherService.getWeather((String) req.getSession().getAttribute("login"), city, apiKey);
+            resp.setContentType("text/plain");
+            resp.getWriter().write(String.format("Temperature: %s", temperature));
+        }
     }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String city = req.getParameter("town");
+//        String apiKey = "ec6ae61f58c44f36d3bd7d4f99c9993a";
+//
+//        String temperature = weatherService.getWeather((String) req.getSession().getAttribute("login"), city, apiKey);
+//
+//        req.setAttribute("temperature", temperature);
+//        req.getRequestDispatcher("weather.jsp").forward(req, resp);
+//    }
 }
--- src/main/webapp/weather.jsp
+++ src/main/webapp/weather.jsp
@@ -4,19 +4,39 @@
 <html>
 <head>
     <title>Weather</title>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script>
+        $(document).on("click", "#ajax-button", function() {
+                $.get("/weather?town=" + $("#town").val(), function (response){
+                    $("#ajax-response").text(response)
+                })
+            }
+        )
+    </script>
 </head>
 <body>
 
-<form method="POST" action="/weather">
+<form>
     <label for="town">Город: </label>
     <input type="text" id="town" name="town" required>
 
-    <button type="submit">Узнать погоду</button>
+    <input type="button" id="ajax-button" value="Узнать погоду">
 </form>
 
-<c:if test="${not empty temperature}">
-    <h3>Температура: ${temperature}</h3>
-</c:if>
+<div id="ajax-response">
+
+</div>
+
+<%--<form method="POST" action="/weather">--%>
+<%--    <label for="town">Город: </label>--%>
+<%--    <input type="text" id="town" name="town" required>--%>
+
+<%--    <button type="submit">Узнать погоду</button>--%>
+<%--</form>--%>
+
+<%--<c:if test="${not empty temperature}">--%>
+<%--    <h3>Температура: ${temperature}</h3>--%>
+<%--</c:if>--%>
 
 </body>
 </html>
